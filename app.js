ebbe1899935343630560617dc51e19d76a73a00c
Corina Andariese <candariese@gmail.com>
2018-04-24 15:48:50 -0700

    Completed first three items on list.

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 window.onload = function() {
   console.log("JavaScript is alive!");
 
-  //please write your code here!
+  document.getElementById('greeting').innerHTML = "Hello World!";
+  document.getElementById('essentials').style.backgroundColor = 'yellow';
+
+  let picture = document.createElement('img');
+  picture.setAttribute("src", "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif");
+  document.getElementById('greeting').appendChild(picture);
+
+  document.getElementById('essentials').addEventListener("click").className = 'selected';
+
+  // var items = document.getElementById("essentials");
+  // for (var i = 0; i < items.length; i++) {
+  //    function() {
+  //     //when clicked, add class select
+  //     //does this remove class select on second click?
+    });
+  }
+
 }
